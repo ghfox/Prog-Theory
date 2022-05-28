c7cb3dacc63163e8ee331a35024cbfc89f4dca29
hwaycock <hwest.aycock@gmail.com>
2022-05-28 01:21:26 -0500

    sturf

---
+++ Assets/Script/ClickHandler.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickHandler : MonoBehaviour
+{
+
+    private Camera GameCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       GameCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = GameCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                var unit = hit.collider.GetComponentInParent<Incendiary>();
+                if (unit != null)
+                    unit.OnClick();
+            }
+        }
+    }
+}
